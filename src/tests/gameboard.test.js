cd9327ad9fa36e4109fcc9605cd8cf8c72359b54
Otroletravaladna <crnana98@gmail.com>
2023-03-23 02:44:24 -0300

    Add tests for Gamboard obj functions

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -6,6 +6,10 @@ describe("Gameboard functions test", () => {
     beforeEach(() => {
         playerShip = new Gameboard("AC", ["a1", "a2", "a3", "a4", "a5"]);
         playerShip.receiveAttack("a2");
+        playerShip.receiveAttack("a3");
+        playerShip.receiveAttack("a4");
+        playerShip.receiveAttack("a5");
+        playerShip.receiveAttack("a1");
     })
 
     test("Return hit message", () => {
@@ -13,6 +17,10 @@ describe("Gameboard functions test", () => {
     })
 
     test("Update hits count of ship", () => {
-        expect(playerShip.ship.hits).toBe(1);
+        expect(playerShip.ship.hits).toBe(5);
+    })
+
+    test("Return sink status of ship", () => {
+        expect(playerShip.reportSink()).toMatch(/This ship is wrecked!/)
     })
 })
