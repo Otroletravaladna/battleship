15e76a8bf9a9fb2cbe06ce5fc83ffb7175681f24
Otroletravaladna <crnana98@gmail.com>
2023-03-26 05:16:23 -0300

    Remove duplicated code

--- src/game-ui.js
+++ src/game-ui.js
@@ -1,14 +1,18 @@
 export function createGrids() {
-    const playerGrid = document.querySelector(".player-grid");
-    const machineGrid = document.querySelector(".machine-grid");
+    const playerGrid = document.querySelector(".player");
+    const machineGrid = document.querySelector(".machine");
 
+    displayGrids(playerGrid);
+    displayGrids(machineGrid);
+}
+
+function displayGrids(parent) {
     for(let i = 0; i < 10; i++) {
         const row = document.createElement("div");
         row.classList.add(`row`);
         row.style.backgroundColor = "white";
         row.textContent = "";
-        playerGrid.append(row);
-
+        parent.append(row);
         for(let j = 0; j < 10; j++) {
             const column = document.createElement("div");
             column.classList.add("column");
