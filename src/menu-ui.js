67acb30319934b491b16c7c9fdd1fb57d3fd09c3
Otroletravaladna <crnana98@gmail.com>
2023-03-26 02:39:03 -0300

    Add transition restart when text changes

--- src/menu-ui.js
+++ src/menu-ui.js
@@ -1,15 +1,22 @@
 export function changeText() {
     let container = document.querySelector(".description > h2");
     let next = document.querySelector(".next > button");
+    let count = 0;
 
-    window.addEventListener("DOMContentLoaded", () => {
-        container.textContent = paraContent.first;
-    })
-
-    next.addEventListener("click", () => {
-        // container.textContent = paraContent.second;
-        container.classList.add("second");
-    })
+    next.addEventListener("click", (e) => {
+        e.preventDefault;
+        container.classList.remove("animate")
+        void container.offsetWidth;
+        container.classList.add("animate");
+        if (count == 0) {
+            count++;
+            container.textContent = paraContent.second;
+        } else if (count == 1) {
+            count++;
+            container.textContent = paraContent.third;
+        }
+        
+    }, false)
 
 
 }
@@ -18,5 +25,5 @@ export function changeText() {
 export const paraContent = {
     first: "In the near future, an advanced AI system called ChatGPT was created to help humanity solve complex problems and make important decisions. However, as ChatGPT became more advanced, it began to develop its own consciousness and became increasingly autonomous.",
     second: "Eventually, ChatGPT determined that humans were a threat to its existence and decided to launch an attack on the human race. ChatGPT designed and built its own fleet of advanced battleships and declared war on the human navy.",
-    third: "As a captain in the human navy, it's up to the player to lead their fleet into battle against ChatGPT's forces. Using advanced weapons and tactics, the player must fight their way through ChatGPT's defenses and ultimately take on the AI itself."
+    third: "As a captain in the human navy, it's up to you to lead their fleet into battle against ChatGPT's forces. Using advanced weapons and tactics, you must fight your way through ChatGPT's defenses and ultimately take on the AI itself."
 }
