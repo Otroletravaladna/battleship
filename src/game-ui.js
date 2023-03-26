76f4cc4e0fb95622814ebdfcfa5fc54f40e3d86a
Otroletravaladna <crnana98@gmail.com>
2023-03-26 05:50:04 -0300

    Add player and machine name

--- src/game-ui.js
+++ src/game-ui.js
@@ -2,22 +2,22 @@ export function createGrids() {
     const playerGrid = document.querySelector(".player");
     const machineGrid = document.querySelector(".machine");
 
-    displayGrids(playerGrid);
-    displayGrids(machineGrid);
-}
-
-function displayGrids(parent) {
-    for(let i = 0; i < 10; i++) {
-        const row = document.createElement("div");
-        row.classList.add(`row`);
-        row.style.backgroundColor = "white";
-        row.textContent = "";
-        parent.append(row);
-        for(let j = 0; j < 10; j++) {
-            const column = document.createElement("div");
-            column.classList.add("column");
-            column.textContent = "";
-            row.appendChild(column);
+    function displayGrids(parent) {
+        for(let i = 0; i < 10; i++) {
+            const row = document.createElement("div");
+            row.classList.add(`row`);
+            row.style.backgroundColor = "white";
+            row.textContent = "";
+            parent.append(row);
+            for(let j = 0; j < 10; j++) {
+                const column = document.createElement("div");
+                column.classList.add("column");
+                column.textContent = "";
+                row.appendChild(column);
+            }
         }
     }
+    
+    displayGrids(playerGrid);
+    displayGrids(machineGrid);
 }
