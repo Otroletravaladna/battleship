33f08a5c40291843a7936de9a1e277120498f325
Otroletravaladna <crnana98@gmail.com>
2023-03-26 06:12:36 -0300

    addcoords to grid item's class

--- src/game-ui.js
+++ src/game-ui.js
@@ -2,16 +2,19 @@ export function createGrids() {
     const playerGrid = document.querySelector(".player");
     const machineGrid = document.querySelector(".machine");
 
+    const y = ['j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'];
+    const x = [1, 2, 3, 4, 5, 6, 7, 8, 9 , 10];
+
     function displayGrids(parent) {
         for(let i = 0; i < 10; i++) {
             const row = document.createElement("div");
-            row.classList.add(`row`);
+            row.classList.add(`column`);
             row.style.backgroundColor = "white";
             row.textContent = "";
             parent.append(row);
             for(let j = 0; j < 10; j++) {
                 const column = document.createElement("div");
-                column.classList.add("column");
+                column.classList.add(`item`,`${y[j] + (i + 1)}`);
                 column.textContent = "";
                 row.appendChild(column);
             }
