1d27d6e8835b232e2d6101e7f036ba950123b0cf
Otroletravaladna <crnana98@gmail.com>
2023-03-28 09:56:47 -0300

    Refactor grid display fn

--- src/game-ui.js
+++ src/game-ui.js
@@ -4,7 +4,18 @@ export function createGrids() {
     const machineGrid = document.querySelector(".machine");
 
     const y = ['j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'];
+    const x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     container.style.display = "grid";
+
+    function displayGrid(parent) {
+        y.forEach((item) => {
+            for (let i = 0; i < 10; i++) {
+                const cell = document.createElement("div");
+                cell.classList.add(item + (i +1));
+                parent.append(cell);
+            }
+        })
+    }
 
     function displayGrids(parent) {
         for(let i = 0; i < 10; i++) {
@@ -22,8 +33,12 @@ export function createGrids() {
         }
     }
     
-    displayGrids(playerGrid);
-    displayGrids(machineGrid);
+    displayGrid(playerGrid);
+    displayGrid(machineGrid);
+    
+    // displayGrids(playerGrid);
+    // displayGrids(machineGrid);
+    dragItem();
 }
 
 export function displayPlayerName() {
@@ -47,15 +62,62 @@ function displayFleetElement() {
 
     function changeAxis() {
         const btn = document.querySelector(".change-axis");
-    
-        btn.onclick = () => container.style.transform += "rotate(90deg)";
+        let isRow = false;
+        
+        btn.addEventListener("click", () => {
+            if (!isRow) {
+                container.style.flexDirection = "row"
+                isRow = true;
+            } else {
+                container.style.flexDirection = "column";
+                isRow = false;
+            };
+        })
     }
 
     changeAxis();
     ac(container);
 }
 
-
-
 displayFleetElement();
+
+function dragItem() {
+    let dragged; 
+
+    const src = document.querySelector(".item-container");
+    src.addEventListener("drag", (e) => {
+        console.log("dragging");
+    })
+
+    src.addEventListener("dragstart", (e) => {
+        dragged = e.target;
+        e.dataTransfer.setDragImage(src, 0, 0);
+        e.target.classList.add("draggin");
+    })
+
+    src.addEventListener("dragend", (e) => {
+        e.target.classList.remove("draggin");
+    })
+
+    const target = document.querySelector(`.player.grid`);
+
+    target.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    }, false);
+
+    target.addEventListener("dragenter", (e) => {
+        e.target.classList.add("dragover");
+        console.log(e.target);
+    })
+
+    target.addEventListener("dragleave", (e) => {
+        e.target.classList.remove("dragover");
+    })
+
+    target.addEventListener("drop", (e) => {
+        e.preventDefault();
+        e.target.classList.remove("dragover");
+        e.target.append(dragged);
+    })
+}
 
