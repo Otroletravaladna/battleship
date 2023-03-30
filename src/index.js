ab5a99fd2d5d4478e1c48a3dd7ed7fe11a938473
Otroletravaladna <crnana98@gmail.com>
2023-03-30 08:51:59 -0300

    Import ia fleet, add test listener

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import * as ships from "./ships";
 import * as menu from "./menu-ui";
 import * as game from "./game-ui";
 import * as data from "./data";
+import * as iaFleet from "./ia-fleet";
 
 menu.changeText();
 
 document.querySelector(".change-axis").addEventListener("click", () => {
     game.changeAxis();
+})
+
+window.addEventListener("click", () => {
+    iaFleet.getMachineFleet();
 })
