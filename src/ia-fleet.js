a5a631abbd9f033cc069fb05e16d6ef0755bdd83
Otroletravaladna <crnana98@gmail.com>
2023-03-30 08:50:57 -0300

    Add get item fn

--- src/ia-fleet.js
+++ src/ia-fleet.js
@@ -1,16 +1,26 @@
+import { y, x } from "./data";
+
 export let shipSize = 5;
 
 export function getMachineFleet() {
     const random = () => Math.floor(Math.random() * 10);
+    let axis = getAxis();
+    let coords = random();
 
-    const getAxis = () => {
-        if (random() % 2 == 0) return "row";
-        return "column";
-    }
-
-    const getItem = (shipSize) => {
-        
+    function getAxis() {
+        return random() % 2 == 0 ? "row" : "column";
     }
     
-    console.log(getAxis())
+    const getItem = (shipSize) => {
+        if (axis == "row") {
+            
+        } else {
+            if (coords < 5) coords += shipSize;
+            for (let i = shipSize; i > 0; i--) {
+                (y[coords - i] + x[coords]);
+            }
+        }
+    }
+    
+    getItem(shipSize);
 }
