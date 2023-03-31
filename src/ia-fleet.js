d6d554826777ca758016937c9370a9018f3c1b4f
Otroletravaladna <crnana98@gmail.com>
2023-03-31 00:39:24 -0300

    Refactor ia fleet array function

--- src/ia-fleet.js
+++ src/ia-fleet.js
@@ -7,54 +7,63 @@ export function getMachineFleet() {
     let axis = getAxis();
     let coords = random();
     let item = [];
+    console.log(shipSize);
+
 
     function getAxis() {
         return random() % 2 == 0 ? "row" : "column";
     }
     
-    const getItem = (shipSize) => {
+    const getItem = (n) => {
         if (axis == "row") {
-            if (coords < shipSize) {
-                for (let i = shipSize; i > 0; i--) {
-                    item.push(y[coords] + x[(coords + shipSize)- i]);
+            if (coords < n) {
+                for (let i = n; i > 0; i--) {
+                    item.push(y[coords] + x[(coords + n)- i]);
                 }
             } else {
-                for (let i = shipSize; i > 0; i--) {
+                for (let i = n; i > 0; i--) {
                     item.push(y[coords] + x[coords- i]);
                 }
             }
 
         } else {
             if (coords < 5){
-                for (let i = shipSize; i > 0; i--) {
-                    item.push(y[(coords + shipSize)- i] + x[coords]);
+                for (let i = n; i > 0; i--) {
+                    item.push(y[(coords + n)- i] + x[coords]);
                 }
             } else {
-                for (let i = shipSize; i > 0; i--) {
-                    item.push(y[coords - i] + x[coords]);
+                for (let i = n; i > 0; i--) {
+                    item.push(y[coords - i] + x[n]);
                 }
             }
         }
+    }
+    if (shipSize == 5) {
         machineCoords.push(item);
-        // checkForVisited(item);
-        console.table(machineCoords);
+        // shipSize--;
+
+    }else if(checkForVisited(item) === false && shipSize > 0) {
+        machineCoords.push(item);
+        // shipSize--;
+        // console.table(machineCoords);
     }
     
-    if(shipSize == 0) return;
+    if (shipSize == 0) return;
     else if (shipSize < 3) getItem(2);
     else getItem(shipSize);
+    console.table(machineCoords);
     shipSize--;
 }
 
-// export function checkForVisited(item) {
-//     console.log("push");
-
-//     item.forEach(e => {
-//         let test = e.map(x => e.includes(x));
-//         // if (!test.includes(true)) machineCoords.push(item);
-//     })
-//     console.table(machineCoords);
-// }
+export function checkForVisited(item) {
+    let test;
+    for (let arr of machineCoords) {
+        // console.table(arr);
+        test = arr.some(x => item.includes(x))
+        console.log(arr.some(x => item.includes(x)));
+    }
+    return test == true ? true : false;
+}
 
 let machineCoords = [
 
